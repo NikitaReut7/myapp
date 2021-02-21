6b1f0380dbf079ef586f38c1d0c4bba255be8902
NikitaReut7 <46726161+NikitaReut7@users.noreply.github.com>
2021-02-21 22:47:26 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Message />
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <div className="App">
+        <ul>
+          <li>Kek</li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+
+const Message = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>Kek</li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+
+export default App;
