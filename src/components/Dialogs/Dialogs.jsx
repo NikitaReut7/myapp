034d82b125cc98a5ddc46b92fbc41531acc798db
NikitaReut7 <46726161+NikitaReut7@users.noreply.github.com>
2021-02-24 13:50:23 +0300

    Added css styles for dialog page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+                <div className={`${classes.dialog} ${classes.active}`}>
+                    Nikita
+                </div>
+                <div className={classes.dialog}>
+                    Ivan
+                </div>
+                <div className={classes.dialog}>
+                    Nastya
+                </div>
+                <div className={classes.dialog}>
+                    Jordan
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>>How are you?</div>
+                <div className={classes.message}>I'm fine, thank you!</div>
+            </div>
         </div>
     );
 }
