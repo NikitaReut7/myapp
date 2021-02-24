ee7b160d83d5738e86ec932ea7e489e3eb09ebe2
Nikita Reut <46726161+NikitaReut7@users.noreply.github.com>
2021-02-24 23:03:36 +0300

    Fixed bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import classes from './Navbar.module.css';
-import NavLink from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-    return(
-        <nav className={classes.nav}>
-            <div className={classes.item}>
-                <NavLink to={"/profile"} activeClassName={classes.activeLink}>Profile</NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to={"/dialogs"} activeClassName={classes.activeLink}>Messages</NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to={"/music"} activeClassName={classes.activeLink}>Music</NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to={"/news"} activeClassName={classes.activeLink}>News</NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to={"/setting"} activeClassName={classes.activeLink}>Setting</NavLink>
-            </div>
-        </nav>
+    return (
+            <nav className={classes.nav}>
+                <div className={classes.item}>
+                    <NavLink to={"/profile"} activeClassName={classes.activeLink}>Profile</NavLink>
+                </div>
+                <div className={classes.item}>
+                    <NavLink to={"/dialogs"} activeClassName={classes.activeLink}>Messages</NavLink>
+                </div>
+                <div className={classes.item}>
+                    <NavLink to={"/music"} activeClassName={classes.activeLink}>Music</NavLink>
+                </div>
+                <div className={classes.item}>
+                    <NavLink to={"/news"} activeClassName={classes.activeLink}>News</NavLink>
+                </div>
+                <div className={classes.item}>
+                    <NavLink to={"/setting"} activeClassName={classes.activeLink}>Setting</NavLink>
+                </div>
+            </nav>
     );
 }
 
